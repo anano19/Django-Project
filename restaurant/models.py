55649afcb30bfcd40e5b2dbc095974ebb3e28f47
anano19 <ana.qoridze.459@gmail.com>
2025-01-17 22:25:11 +0400

    initial commit

--- restaurant/models.py
+++ restaurant/models.py
@@ -19,3 +19,12 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"Booking for {self.name} on {self.booking_date}"
+    
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MenuItem, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
